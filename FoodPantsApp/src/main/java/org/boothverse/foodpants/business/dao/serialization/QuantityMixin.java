2b3a6e09f4f30f6e7f1a3f8044b5de4a60ceec8e
daniel-luper <jdaniel.luper@gmail.com>
2022-04-27 17:26:50 -0500

    resolve merge conflicts

--- FoodPantsApp/src/main/java/org/boothverse/foodpants/business/dao/serialization/QuantityMixin.java
+++ FoodPantsApp/src/main/java/org/boothverse/foodpants/business/dao/serialization/QuantityMixin.java
@@ -1,9 +1,7 @@
 package org.boothverse.foodpants.business.dao.serialization;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 @JsonDeserialize(using = QuantityDeserializer.class)
-//@JsonSerialize(using = QuantitySerializer.class)
 public abstract class QuantityMixin {
 }
