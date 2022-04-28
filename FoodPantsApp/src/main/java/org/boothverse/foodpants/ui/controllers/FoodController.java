bf20fe070267a6348f0d2c005dc1e252e85bce5c
lukalelovic <lukalelovic@gmail.com>
2022-04-28 10:23:59 -0500

    NutritionController and FoodController implementation :0

--- FoodPantsApp/src/main/java/org/boothverse/foodpants/ui/controllers/FoodController.java
+++ FoodPantsApp/src/main/java/org/boothverse/foodpants/ui/controllers/FoodController.java
@@ -1,30 +1,62 @@
 package org.boothverse.foodpants.ui.controllers;
 
+import org.boothverse.foodpants.business.services.Services;
 import org.boothverse.foodpants.persistence.Food;
 import org.boothverse.foodpants.persistence.FoodGroup;
 import org.boothverse.foodpants.persistence.NutritionDescriptor;
 
 import java.util.List;
-import java.util.Map;
 
 public class FoodController {
+    /**
+     *
+     * @param id
+     * @return
+     */
     public Food getFood(String id) {
         return null;
     }
 
+    /**
+     *
+     * @param name
+     * @param category
+     * @param nutrition
+     * @return
+     */
     public Food addFood(String name, FoodGroup category, NutritionDescriptor nutrition) {
-        return null;
+        Food food = new Food(Services.ID_SERVICE.getId(), name, category, nutrition);
+        Services.FOOD_SERVICE.addFood(food);
+        return food;
     }
 
+    /**
+     *
+     * @param id
+     * @param name
+     * @param category
+     * @param nutrition
+     * @return
+     */
     public Food editFood(String id, String name, FoodGroup category, NutritionDescriptor nutrition) {
-        return null;
+        Food food = new Food(id, name, category, nutrition);
+        Services.FOOD_SERVICE.editFood(food);
+        return food;
     }
 
-    public Food removeFood(String id) {
-        return null;
+    /**
+     *
+     * @param id
+     */
+    public void removeFood(String id) {
+        Services.FOOD_SERVICE.removeFood(id);
     }
 
+    /**
+     *
+     * @return
+     */
     public List<Food> getFoods() {
-        return null;
+        return Services.FOOD_SERVICE.getFoods();
     }
 }
