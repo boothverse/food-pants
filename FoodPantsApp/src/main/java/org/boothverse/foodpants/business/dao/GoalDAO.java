b6bc0f4388178238ec28a59e2591eb0e71dd641d
PJWallace1 <90213252+PJWallace1@users.noreply.github.com>
2022-04-28 11:47:25 -0500

    GoalDAO implemented and tested

--- FoodPantsApp/src/main/java/org/boothverse/foodpants/business/dao/GoalDAO.java
+++ FoodPantsApp/src/main/java/org/boothverse/foodpants/business/dao/GoalDAO.java
@@ -1,25 +1,62 @@
 package org.boothverse.foodpants.business.dao;
 
+import org.boothverse.foodpants.business.dao.util.SQLUtils;
 import org.boothverse.foodpants.persistence.Goal;
+import org.boothverse.foodpants.persistence.GoalType;
+import org.boothverse.foodpants.persistence.NutritionInstance;
+import org.boothverse.foodpants.persistence.NutritionType;
+import tec.units.ri.quantity.Quantities;
 
+import javax.measure.Quantity;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 public class GoalDAO extends JDBCListDAO<Goal> {
+    /**
+     * Constructor for NutritionInstanceDAO
+     */
     GoalDAO() {
         super("goals", new String[]{"id", "goalType", "dailyQuantity", "nutritionType"});
     }
 
+    /**
+     * Converts object data to SQL
+     *
+     * @param data
+     * @return
+     */
     @Override
     protected String[] objToSQL(Goal data) {
-        // TODO
-        return new String[0];
+        return new String[]{
+            SQLUtils.inQuote(data.getId()),
+            SQLUtils.inQuote(data.getGoalType().toString()),
+            SQLUtils.inQuote(data.getDailyQuantity().toString()),
+            SQLUtils.inQuote(data.getNutritionType().toString())
+        };
     }
 
+    /**
+     * Converts SQL to object data
+     *
+     * @param rs
+     * @return
+     * @throws SQLException
+     */
     @Override
     protected Map<String, Goal> SQLToObj(ResultSet rs) throws SQLException {
-        // TODO
-        return null;
+        Map<String, Goal> data = new HashMap<>();
+
+        while (rs.next()) {
+            String id = rs.getString(1);
+            GoalType goalType = GoalType.valueOf(rs.getString(2));
+            Quantity<?> quantity = Quantities.getQuantity(rs.getString(3));
+            NutritionType nutritionType = NutritionType.valueOf(rs.getString(4));
+
+            data.put(id, new Goal(id, goalType, quantity, nutritionType));
+        }
+        return data;
     }
 }
