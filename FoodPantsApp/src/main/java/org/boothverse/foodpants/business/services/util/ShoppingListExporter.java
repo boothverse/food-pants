010686ba9547d62ff74ce86ef29a39c574fd70d2
daniel-luper <jdaniel.luper@gmail.com>
2022-04-29 11:17:38 -0500

    shoppinglist exporter with data

--- FoodPantsApp/src/main/java/org/boothverse/foodpants/business/services/util/ShoppingListExporter.java
+++ FoodPantsApp/src/main/java/org/boothverse/foodpants/business/services/util/ShoppingListExporter.java
@@ -4,39 +4,36 @@ import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import org.boothverse.foodpants.business.services.exceptions.PantsExportShoppingListException;
-import org.boothverse.foodpants.persistence.FoodInstance;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class ShoppingListExporter {
 
+    private static final String FONT = FontFactory.HELVETICA_BOLD;
+
     public record ShoppingListItem(String name, String quantity) {}
 
-    public void export(Path path, List<FoodInstance> items) throws PantsExportShoppingListException {
+    public void export(Path path, List<ShoppingListItem> items) throws PantsExportShoppingListException {
         Document document = new Document();
         try {
             PdfWriter.getInstance(document, new FileOutputStream(path.toString()));
             document.open();
 
             // Title
-            Font titleFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD, 18, BaseColor.BLACK);
+            Font titleFont = FontFactory.getFont(FONT, 18, BaseColor.BLACK);
             Paragraph title = new Paragraph("Shopping List", titleFont);
             title.setAlignment(Element.ALIGN_CENTER);
             document.add(title);
             document.add(new Paragraph("\n"));
 
             // Content
-            //PdfPTable table = new PdfPTable(2);
-            //addRows(table);
-            //addRows(table);
-            //document.add(table);
+            PdfPTable table = new PdfPTable(2);
+            addRows(table, items);
+            document.add(table);
 
             document.close();
         } catch (FileNotFoundException | DocumentException e) {
@@ -46,15 +43,20 @@ public class ShoppingListExporter {
     }
 
     private void addRows(PdfPTable table, List<ShoppingListItem> items) {
+        Font font = FontFactory.getFont(FONT, 12, BaseColor.BLACK);
         items.forEach(item -> {
             PdfPCell quantity = new PdfPCell();
             quantity.setBorderColor(BaseColor.WHITE);
-            quantity.setPhrase(new Phrase(item.quantity));
+            Phrase p1 = new Phrase(item.quantity);
+            p1.setFont(font);
+            quantity.setPhrase(p1);
             table.addCell(quantity);
 
             PdfPCell name = new PdfPCell();
-            quantity.setBorderColor(BaseColor.WHITE);
-            quantity.setPhrase(new Phrase(item.name));
+            name.setBorderColor(BaseColor.WHITE);
+            Phrase p2 = new Phrase(item.name);
+            p2.setFont(font);
+            name.setPhrase(p2);
             table.addCell(name);
         });
     }
