0f1c1aaec179ffb92a3031d5c6eb62bf2648c967
patrickeharris <patrickeharris404@gmail.com>
2022-04-30 13:24:59 -0500

    Implement food tester

---
+++ FoodPantsApp/src/test/java/org/boothverse/foodpants/persistence/FoodTester.java
@@ -0,0 +1,33 @@
+package org.boothverse.foodpants.persistence;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import tech.units.indriya.quantity.Quantities;
+import tech.units.indriya.unit.Units;
+
+import javax.measure.Quantity;
+import javax.measure.quantity.Mass;
+
+public class FoodTester {
+    NutritionDescriptor nd = new NutritionDescriptor();
+    Food food = new Food("1", "1", FoodGroup.FRUIT, nd);
+
+    @Test
+    public void createFoodWithEmptyNutritionDescriptor(){
+        Assertions.assertEquals(0,food.getNutrition().getNutritionInfo().size());
+        Assertions.assertNull(food.getNutrition().getServingSize());
+    }
+
+    @Test
+    public void createInstanceWithNullQuantity(){
+        FoodInstance instance = food.createInstance(null);
+        Assertions.assertNull(instance.getQuantity());
+    }
+
+    @Test
+    public void createInstance(){
+        Quantity<Mass> quantity = Quantities.getQuantity(5, Units.KILOGRAM);
+        FoodInstance instance = food.createInstance(quantity);
+        Assertions.assertEquals(5, instance.getQuantity().getValue());
+    }
+}
