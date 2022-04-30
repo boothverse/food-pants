847f1593289f73c40dd9466cf7ec9293de625538
patrickeharris <patrickeharris404@gmail.com>
2022-04-30 13:31:32 -0500

    Create function stubs for persistence layer unit tests

--- FoodPantsApp/src/test/java/org/boothverse/foodpants/persistence/GoalTester.java
+++ FoodPantsApp/src/test/java/org/boothverse/foodpants/persistence/GoalTester.java
@@ -1,4 +1,15 @@
 package org.boothverse.foodpants.persistence;
 
+import org.junit.jupiter.api.Test;
+
 public class GoalTester {
+    @Test
+    public void checkGoalAttained(){
+
+    }
+
+    @Test
+    public void checkGoalAttainedNullQuantity(){
+
+    }
 }
--- FoodPantsApp/src/test/java/org/boothverse/foodpants/persistence/NutritionInstanceTester.java
+++ FoodPantsApp/src/test/java/org/boothverse/foodpants/persistence/NutritionInstanceTester.java
@@ -1,4 +1,15 @@
 package org.boothverse.foodpants.persistence;
 
+import org.junit.jupiter.api.Test;
+
 public class NutritionInstanceTester {
+    @Test
+    public void checkToString(){
+
+    }
+
+    @Test
+    public void checkToStringWithNullFields(){
+
+    }
 }
