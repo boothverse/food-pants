423d5cf94ef2bdd2f12cf11ee4e8740a78048bf4
patrickeharris <patrickeharris404@gmail.com>
2022-05-01 15:52:52 -0500

    Nutrition instance testing

--- FoodPantsApp/src/test/java/org/boothverse/foodpants/persistence/NutritionInstanceTester.java
+++ FoodPantsApp/src/test/java/org/boothverse/foodpants/persistence/NutritionInstanceTester.java
@@ -1,15 +1,27 @@
 package org.boothverse.foodpants.persistence;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import tech.units.indriya.quantity.Quantities;
+import tech.units.indriya.unit.Units;
+
+import javax.measure.Quantity;
+import javax.measure.quantity.Mass;
+import java.time.Instant;
+import java.util.Date;
 
 public class NutritionInstanceTester {
+    Quantity<Mass> quantity = Quantities.getQuantity(5, Units.KILOGRAM);
+    Date consumedAt = Date.from(Instant.now());
+    NutritionInstance nullFields = new NutritionInstance();
+    NutritionInstance nutritionInstance = new NutritionInstance("1", "banana", quantity, consumedAt);
     @Test
     public void checkToString(){
-
+        Assertions.assertEquals(nutritionInstance.toString(), "NutritionInstance(id=1, quantity=5 kg, foodId=banana, consumedAt=" + consumedAt.toString() +")");
     }
 
     @Test
     public void checkToStringWithNullFields(){
-
+        Assertions.assertEquals(nullFields.toString(), "NutritionInstance(id=null, quantity=null, foodId=null, consumedAt=null)");
     }
 }
