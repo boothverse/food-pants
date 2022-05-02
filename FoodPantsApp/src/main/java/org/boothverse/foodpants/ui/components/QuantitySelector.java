49d1beb0461afac3379c742c681d51df6501edd5
Austin Huizinga <austinth127@gmail.com>
2022-05-02 18:04:51 -0500

    Small changes

--- FoodPantsApp/src/main/java/org/boothverse/foodpants/ui/components/QuantitySelector.java
+++ FoodPantsApp/src/main/java/org/boothverse/foodpants/ui/components/QuantitySelector.java
@@ -1,12 +1,10 @@
 package org.boothverse.foodpants.ui.components;
 
 import lombok.Getter;
-import lombok.Setter;
 import systems.uom.unicode.CLDR;
 import tech.units.indriya.AbstractUnit;
 import tech.units.indriya.quantity.Quantities;
 import tech.units.indriya.unit.Units;
-
 import javax.measure.Quantity;
 import javax.measure.Unit;
 import javax.swing.*;
@@ -19,6 +17,7 @@ public class QuantitySelector extends JPanel {
     public static final String[] quantityOptions = {"unit", "g", "kg", "oz", "lb", "fl oz", "cup", "gallon", "L", "calorie"};
     public static final Unit<?>[] unitClasses = {AbstractUnit.ONE, Units.GRAM, Units.KILOGRAM, CLDR.OUNCE, CLDR.POUND,
         CLDR.FLUID_OUNCE, CLDR.CUP, CLDR.GALLON, Units.LITRE, CLDR.CALORIE};
+
     protected JComboBox<String> quantityUnitBox;
     protected JFormattedTextField quantityValueField;
 
