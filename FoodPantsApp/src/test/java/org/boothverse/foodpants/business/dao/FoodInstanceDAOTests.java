87a7b6d1e6d4b69a848d6b553e6bf787a7ff402e
daniel-luper <jdaniel.luper@gmail.com>
2022-05-02 13:16:26 -0500

    load data for test wow so cool

--- FoodPantsApp/src/test/java/org/boothverse/foodpants/business/dao/FoodInstanceDAOTests.java
+++ FoodPantsApp/src/test/java/org/boothverse/foodpants/business/dao/FoodInstanceDAOTests.java
@@ -1,5 +1,6 @@
 package org.boothverse.foodpants.business.dao;
 
+import org.boothverse.foodpants.business.dao.util.JDBCUtils;
 import org.boothverse.foodpants.persistence.FoodInstance;
 import org.junit.jupiter.api.*;
 import tech.units.indriya.quantity.Quantities;
@@ -7,26 +8,19 @@ import tech.units.indriya.unit.Units;
 
 import javax.measure.Quantity;
 import javax.measure.quantity.Mass;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FoodInstanceDAOTests extends BaseDAOTests {
 
-    static List<FoodInstance> backup;
     static ListDAO<FoodInstance> pantry;
 
     @BeforeAll
     static void setup() {
-        // Clear table
         pantry = new FoodInstanceDAO("pantry");
-        backup = new ArrayList<>(pantry.load().values());
-        pantry.removeAll();
-
-        initIds();
+        setup(pantry);
+        executeScript("pantry_01.sql");
     }
 
     @Test
@@ -42,11 +36,10 @@ public class FoodInstanceDAOTests extends BaseDAOTests {
     @Test
     @Order(2)
     void foodInstanceLoadTest() {
-        ListDAO<FoodInstance> dao = new FoodInstanceDAO("PANTRY");
         Quantity<Mass> quantity = Quantities.getQuantity(50, Units.GRAM);
 
-        Map<String, FoodInstance> foodInstances = dao.load();
-        assertEquals(1, foodInstances.size());
+        Map<String, FoodInstance> foodInstances = pantry.load();
+        assertEquals(2, foodInstances.size());
 
         FoodInstance foodInstance = foodInstances.get(testIds.get(0));
 
